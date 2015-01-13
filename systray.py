04421354572e5cdd94a308d1170bfecf054548b4
queeup <ozgur.baskin@gmail.com>
2015-01-13 13:29:08 +0200

    added more info & activate/deactivate Item support

--- systray.py
+++ systray.py
@@ -4,7 +4,10 @@ import os
 import sys
 import gtk
 
-# If pulseaudio_dlna.py not in the same directory please edit this line for your needs
+# If pulseaudio_dlna.py not in the same directory
+# or
+# if you need to add some options to pulseaudio_dlna.py
+# please edit this line for your needs.
 pulseaudio_dlna = sys.path[0] + '/pulseaudio_dlna.py'
 
 
@@ -25,6 +28,7 @@ class MainClass:
     self.menu = gtk.Menu()
     servicesMenuItem = gtk.ImageMenuItem()
     
+    # Label Item
     title = gtk.Label()
     title.set_text("<b><span foreground=\"grey\">" + "Pulseaudio DLNA Server:" + "</span></b>")
     title.set_justify(gtk.JUSTIFY_LEFT)
@@ -32,28 +36,47 @@ class MainClass:
     title.set_use_markup(True)
     servicesMenuItem.add(title)
     self.menu.append(servicesMenuItem)
-    startMenuItem = gtk.MenuItem('Start')
-    startMenuItem.connect('activate', self._start)
-    self.menu.append(startMenuItem)
-
+    
+    # Start Item
+    self.startMenuItem = gtk.MenuItem('Start')
+    self.startMenuItem.connect('activate', self._start)
+    self.menu.append(self.startMenuItem)
+    
+    # Seperator Item
     self.menu.append(gtk.SeparatorMenuItem())
-
-    stopMenuItem = gtk.MenuItem('Stop')
-    stopMenuItem.connect('activate', self._stop)
-    self.menu.append(stopMenuItem)
-
+    
+    # Stop Item
+    self.stopMenuItem = gtk.MenuItem('Stop')
+    self.stopMenuItem.connect('activate', self._stop)
+    # Deactivate Stop Item at the beginning. Cosmetic
+    self.stopMenuItem.set_sensitive(False)
+    self.menu.append(self.stopMenuItem)
+    
+    # Seperator Item
     self.menu.append(gtk.SeparatorMenuItem())
-
+    
+    # Quit Item
     menuItem = gtk.ImageMenuItem(gtk.STOCK_QUIT)
     menuItem.connect('activate', self.quit_cb)
     self.menu.append(menuItem)
+    
     self.menu.show_all()
 
   def _start(self, widget):
+    # Activate Stop Item first. Deactivated on beginning.
+    self.stopMenuItem.set_sensitive(True)
+    # Deactivate Start Item after starting to avoid confuse and start second instant
+    self.startMenuItem.set_sensitive(False)
+    # Load pulseaudio module-dbus-protocol
     os.system('pactl load-module module-dbus-protocol')
+    # execute on backround
     os.system('%s &' % pulseaudio_dlna)
   
   def _stop(self, widget):
+    # Deactivate Stop Item after stoping.
+    self.stopMenuItem.set_sensitive(False)
+    # Activate Start Item after stoping.
+    self.startMenuItem.set_sensitive(True)
     os.system('kill -TERM `pidof -x pulseaudio_dlna.py`')
 
   def quit_cb(self, widget):
